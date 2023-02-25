e04d054a1ecc0639ed8a1fd9ef2802efd1ce573c
Samuel Wilk <samuel.wilk.00@gmail.com>
2023-02-25 02:54:55 +0100

    Login only with specific role
    Added allow login only with role, added changes from other pull requests, updated and tested dependency

--- AuthOpenIDConnect.php
+++ AuthOpenIDConnect.php
@@ -2,84 +2,112 @@
     require_once(__DIR__."/vendor/autoload.php");
     use Jumbojett\OpenIDConnectClient;
 
-    class AuthOpenIDConnect extends AuthPluginBase {
+    class AuthOpenIDConnect extends LimeSurvey\PluginManager\AuthPluginBase {
         protected $storage = 'DbStorage';
-        protected $settings = [
-            'info' => [
+        protected $settings = array(
+            'info' => array(
                 'type' => 'info',
                 'content' => '<h1>OpenID Connect</h1><p>Please provide the following settings.</br>If necessary settings are missing, the default authdb login will be shown.</p>'
-            ],
-            'providerURL' => [
+            ),
+            'providerURL' => array(
                 'type' => 'string',
                 'label' => 'Provider URL',
-                'help' => 'Required',
+                'help' => 'Required.',
                 'default' => ''
-            ],
-            'clientID' => [
+            ),
+            'clientID' => array(
                 'type' => 'string',
                 'label' => 'Client ID',
-                'help' => 'Required',
+                'help' => 'Required.',
                 'default' => ''
-            ],
-            'clientSecret' => [
+            ),
+            'clientSecret' => array(
                 'type' => 'string',
                 'label' => 'Client Secret',
-                'help' => 'Required',
+                'help' => 'Required.',
                 'default' => ''
-            ],
-            'redirectURL' => [
+	    ),
+            'redirectURL' => array(
                 'type' => 'string',
                 'label' => 'Redirect URL',
                 'help' => 'The Redirect URL is automatically set on plugin activation.',
                 'default' => '',
-                'htmlOptions' => [
+                'htmlOptions' => array(
                     'readOnly' => true,          
-                ]
-            ]
-        ];
+                )
+            ),
+	    'roleBasedLogin' => array(
+	        'type' => 'boolean',
+		'label' => 'Activate role based login',
+		'help' => 'Only users with specific role can log in.',
+	        'default' => false,
+	    ),
+	    'roleClaimName' => array(
+	        'type' => 'string',
+		'label' => 'Role Claim Name',
+		'help' => 'Name of the claim containing the active role. Can\'t login without active role.',
+	        'default' => 'roles',
+	    ),
+	    'roleName' => array(
+	        'type' => 'string',
+		'label' => 'Role Name',
+		'help' => 'Name of the necessary role to log in.',
+		'default' => 'active',
+	    ),
+        );
         static protected $description = 'OpenID Connect Authenticaton Plugin for LimeSurvey.';
         static protected $name = 'AuthOpenIDConnect';
 
         public function init(){
             $this->subscribe('beforeActivate');
-            $this->subscribe('beforeLogin');
-            $this->subscribe('newUserSession');
+	    $this->subscribe('beforeLogin');
+	    $this->subscribe('newUserSession');
             $this->subscribe('afterLogout');
-        }
+	}
 
         public function beforeActivate(){
             $baseURL = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . "{$_SERVER['HTTP_HOST']}";
             $basePath = preg_split("/\/pluginmanager/", $_SERVER['REQUEST_URI']);
             $this->set('redirectURL', $baseURL . $basePath[0] . "/authentication/sa/login");
-        }
+	}
 
         public function beforeLogin(){
             $providerURL = $this->get('providerURL', null, null, false);
             $clientID = $this->get('clientID', null, null, false);
             $clientSecret = $this->get('clientSecret', null, null, false);
             $redirectURL = $this->get('redirectURL', null, null, false);
-
-            if(!$providerURL || !$clientSecret || !$clientID || !$redirectURL){
+	    $roleBasedLogin = $this->get('roleBasedLogin', null, null, false);
+	    	    
+	    if(!$providerURL || !$clientSecret || !$clientID || !$redirectURL){
                 // Display authdb login if necessary plugin settings are missing.
                 return;
             }
-
+            
             $oidc = new OpenIDConnectClient($providerURL, $clientID, $clientSecret);
             $oidc->setRedirectURL($redirectURL);
-            
+
             if(isset($_REQUEST['error'])){
                 return;
             }
 
             try {
-                if($oidc->authenticate()){
-                    $username = $oidc->requestUserInfo('preferred_username');
-                    $email = $oidc->requestUserInfo('email');
-                    $givenName = $oidc->requestUserInfo('given_name');
-                    $familyName = $oidc->requestUserInfo('family_name');
-    
+		if($oidc->authenticate()){
+		    $user_data = $oidc->requestUserInfo();
+		    $username = $user_data->preferred_username;
+		    $email = $user_data->email;
+		    $givenName = $user_data->given_name;
+		    $familyName = $user_data->family_name;
+		    if ($roleBasedLogin == true) {
+		        $claimName = $this->get('roleClaimName', null, null, false);
+		    	$roleName = $this->get('roleName', null, null, false);
+		    	$roles = $user_data->$claimName;
+		        if (!is_array($roles) || !in_array($roleName, $roles)) {
+		    	    // No active group  
+		    	    return;
+		        }
+		    }
                     $user = $this->api->getUserByName($username);
-    
+    		    
                     if(empty($user)){
                         $user = new User;
                         $user->users_name = $username;
@@ -95,7 +123,6 @@
                         }
                         // User successfully created.
                     }
-    
                     $this->setUsername($user->users_name);
                     $this->setAuthPlugin();
                     return;
